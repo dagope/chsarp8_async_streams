e6f879d2274dc8ac10c93558b2c2f95d88abc928
David Gonzalo <dagope@gmail.com>
2019-02-17 17:01:34 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Async streams C# 8.0 ");            
+            Console.WriteLine("Start - using tasks with IEnumerable in C# 7.0 ");
+
+            foreach(var data in await GetBigResultsWithCsarp7Async())
+            {
+                Console.WriteLine($"{DateTime.Now.ToString()}  => {data}");
+            }
+        
+            Console.WriteLine("End - using tasks with IEnumerable in C# 7.0 ");
+            
+            Console.WriteLine("Start - using tasks with IAsyncEnumerable in C# 8.0 ");
+
+            await foreach(var data in GetBigResultsWithCsarp8Async())
+            {
+                Console.WriteLine($"{DateTime.Now.ToString()}  => {data}");
+            }            
+            
+            Console.WriteLine("End - using tasks with IAsyncEnumerable in C# 8.0 ");
+        }
+        
+        static async Task<IEnumerable<int>> GetBigResultsWithCsarp7Async()
+        {
+            List<int> dataPoints = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(1000); //Simulate waiting for external API
+                dataPoints.Add(i);
+            }
+        
+            return dataPoints;
+        }
+        
+        static async IAsyncEnumerable<int> GetBigResultsWithCsarp8Async()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(1000); //Simulate waiting for external API
+                yield return i;
+            }
+        }
+        
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Run with docker
+You can run docker image with see the results. Only execute:
+```bash
+> docker-compose up
+```
+
+# Bonus
+You can read the post about C# 8.0 news in my blog [Spanish]:
+[https://dagope.github.io/2019/02/19/novedades-csharp8](https://dagope.github.io/2019/02/19/novedades-csharp8)
